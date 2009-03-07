19eade043bddea84f20143ec739c383d81d02f05
Paul Battley <pbattley@gmail.com>
2009-03-07 14:22:06 +0000

    The explanations of the series labels are now under the graph.

--- ons_graphs.user.js
+++ ons_graphs.user.js
@@ -6,17 +6,18 @@
 
 ONSGraphs = {
   loadScripts: function(){
-    var GM_JQ = document.createElement('script');  
-    GM_JQ.src = 'http://po-ru.com/ons-graph/monster.js';  
-    GM_JQ.type = 'text/javascript';  
-    document.getElementsByTagName('head')[0].appendChild(GM_JQ);  
+    var GM_JQ = document.createElement('script');
+    GM_JQ.src = 'http://po-ru.com/ons-graph/monster.js';
+    GM_JQ.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(GM_JQ);
   },
 
-  waitForScriptsToLoad: function() {  
-    if(typeof unsafeWindow.jQuery == 'undefined') { 
+  waitForScriptsToLoad: function() {
+    if(typeof unsafeWindow.jQuery == 'undefined') {
       window.setTimeout(ONSGraphs.waitForScriptsToLoad,100);
     } else {
       ONSGraphs.drawGraph();
+      ONSGraphs.addLabelTranslations();
     }
   },
 
@@ -26,26 +27,41 @@ ONSGraphs = {
   },
 
   drawGraph: function(){
-    ONSGraphs.dataTableElement().graphTable({series: 'columns', position: 'before', height: '400', labelTransform: ONSGraphs.mapSeriesLabel()});
+    ONSGraphs.dataTableElement().graphTable({ series: 'columns',
+                                              position: 'before',
+                                              height: '400' });
+  },
+
+  addLabelTranslations: function(){
+    var table = document.createElement('table');
+    unsafeWindow.jQuery.each(ONSGraphs.seriesLabelTranslations(), function(key, value){
+      var tr = document.createElement('tr');
+      var th = document.createElement('th');
+      th.appendChild(document.createTextNode(key));
+      tr.appendChild(th);
+      var td = document.createElement('td');
+      td.appendChild(document.createTextNode(value));
+      tr.appendChild(td);
+      table.appendChild(tr);
+    });
+    ONSGraphs.dataTableElement().before(table);
   },
 
   dataTableElement: function(){
     return unsafeWindow.jQuery('table[rules=all]');
   },
 
-  mapSeriesLabel: function(){
-    var mapping = {};
+  seriesLabelTranslations: function(){
+    var translations = {};
     var $ = unsafeWindow.jQuery;
     $('p>table>tbody>tr').each(function(){
       var key = $(this).find('td:first').text();
       if (key != ''){
         var value = $(this).find('td:last').text();
-        mapping[key] = value;
+        translations[key] = value;
       }
     });
-    return function(key){
-      return mapping[key] || '???';
-    }
+    return translations;
   }
 }
 ONSGraphs.go();
